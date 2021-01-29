2a7d7f4f6f7d30370d17fa4743fd01139c2c4921
ccarver80 <ckc_80@outlook.com>
2021-01-28 23:10:57 -0700

    cc

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,30 @@ $( document ).ready(function() {
   $('.hover').hover(function() {
     $('.text').css('display', 'block');
   }, function() {
-    $('.text').css('display', 'block');
+    $('.text').css('display', 'none');
   });
 
+  $('.hover1').hover(function() {
+    $('.text1').css('display', 'block');
+  }, function() {
+    $('.text1').css('display', 'none');
+  });
+
+  $('.hover2').hover(function() {
+    $('.text2').css('display', 'block');
+  }, function() {
+    $('.text2').css('display', 'none');
+  });
+
+  $('.hover3').hover(function() {
+    $('.text3').css('display', 'block');
+  }, function() {
+    $('.text3').css('display', 'none');
+  });
+
+  $('.hover4').hover(function() {
+    $('.text4').css('display', 'block');
+  }, function() {
+    $('.text4').css('display', 'none');
+  });
 });
