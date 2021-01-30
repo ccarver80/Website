9978d721b6d63e930509601826f465e7ec735431
ccarver80 <ckc_80@outlook.com>
2021-01-29 20:54:37 -0700

    cc

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
+
+  //Slide in Animations
+
+  $('#profile').hide().delay(1000).slideDown();
+
+  $('#name').hide().delay(2000).slideDown();
+
+  $('#toolbox').hide().delay(3000).slideDown();
+
+
+  //Hover Animations
+
   $('.hover').hover(function() {
     $('.text').css('display', 'block');
   }, function() {
