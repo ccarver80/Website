a5fad75d98b65f1ef82625ca0940c9a904604626
Chris <ckc_80@outlook.com>
2021-05-29 08:10:01 -0600

    cc

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('menu');
+
+function menuCLick() {
+  if (menu.style.display == "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
