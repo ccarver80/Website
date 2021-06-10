9a24cd6de2fecfef5a881f41e57751804c7a2c51
Chris <ckc_80@outlook.com>
2021-06-09 21:03:59 -0600

    Got first function button to kind of work

--- js/index.js
+++ js/index.js
@@ -1,8 +1,52 @@
 const terminal = document.getElementById('terminalContainer');
+const home = document.getElementById('home');
+const inputField = document.getElementById('terminalText');
+const homeButton = document.getElementById('homeButton');
+const projectButton = document.getElementById('projects');
+const contactButton = document.getElementById('contact');
+const stackButton = document.getElementById('stack');
+const runButton = document.getElementById('run');
+const displayDiv = document.getElementById('display');
+
+inputField.value ="";
 
 
 function displayTerminal() {
   terminal.style.display = 'block';
 }
+ setInterval(displayTerminal, 2000);
 
-setInterval(displayTerminal, 2000);
+
+function homePage() {
+  home.style.display = 'block';
+}
+
+
+
+homeButton.addEventListener('click', () => {
+  inputField.value ="Profile_Home";
+})
+
+projectButton.addEventListener('click', () => {
+  inputField.value ="Profile(Projects)";
+})
+
+contactButton.addEventListener('click', () => {
+  inputField.value ="Profile(Contact)";
+})
+
+stackButton.addEventListener('click', () => {
+  inputField.value ="Profile(Stack)";
+})
+
+runButton.addEventListener('click', () => {
+  let id = inputField.value.toLowerCase();
+  let x = document.getElementById(`${id}`);
+  x.style.display = "block";
+})
+
+
+
+
+
+
